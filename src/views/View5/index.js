0de361d4ee0ffd42ffd04f8a05a91285692412c2
hanant3 <hanant3@uic.edu>
2023-11-28 09:43:14 -0600

    changes

--- src/views/View5/index.js
+++ src/views/View5/index.js
@@ -9,7 +9,7 @@ export default class View5 extends Component {
             <div id='view5' className='pane'>
                 <div className='header'>AGE</div>
                 <div style={{ overflowX: 'scroll',overflowY:'hidden' }}>
-                <BarChart data={data} width={1000} height={550}/>
+                <BarChart data={data} width={1000} height={550} />
                 </div>                
             </div>
         )
